f56b5e351761c92d82a3cbe96fb786d53844e749
이현승 <77336664+gesal03@users.noreply.github.com>
2022-11-18 13:46:01 +0900

    image capture
    d

--- letterOnTree/letterOnTree/urls.py
+++ letterOnTree/letterOnTree/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from letterOnTreeApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('test', views.canvasToImage, name="test"),
 ]
