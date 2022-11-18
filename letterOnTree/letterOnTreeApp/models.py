83381838bc0145b6697fefdf6f90407bfeebaa30
이현승 <77336664+gesal03@users.noreply.github.com>
2022-11-18 18:38:15 +0900

    model에 이미지 경로 넣기

--- letterOnTree/letterOnTreeApp/models.py
+++ letterOnTree/letterOnTreeApp/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Letter(models.Model):
-    title = models.TextField(max_length=50)
-    image = models.ImageField(blank=True, null=True, upload_to='')
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
